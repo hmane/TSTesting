13eb1fbfdbd71157f8e9d9ee7445ed3139158500
Hemant Mane <hemant.u.mane@gmail.com>
2025-09-03 16:27:26 -0700

    Add Description component with tooltip support

---
+++ field/components/Description.tsx
@@ -0,0 +1,59 @@
+import React, { memo, useMemo } from 'react';
+import { TooltipHost, Icon } from '@fluentui/react';
+import type { ITooltipHostStyles } from '@fluentui/react/lib/Tooltip';
+import type { DescriptionProps } from '../Field.types';
+import styles from '../Field.module.scss';
+
+const tooltipStyles: Partial<ITooltipHostStyles> = {
+	root: {
+		display: 'inline-flex',
+		alignItems: 'center',
+	},
+};
+
+export const Description = memo<DescriptionProps>(({
+	children,
+	icon = 'Info',
+	position = 'end',
+	delay = 300,
+	className = '',
+	style,
+}) => {
+	const descriptionClasses = useMemo(() => {
+		const classes = [styles.description];
+		
+		// Add position class if needed
+		if (position === 'inline') {
+			classes.push(styles.descriptionInline);
+		}
+		
+		if (className) {
+			classes.push(className);
+		}
+		
+		return classes.join(' ');
+	}, [position, className]);
+
+	const tooltipProps = useMemo(() => ({
+		content: children,
+		delay,
+		styles: tooltipStyles,
+	}), [children, delay]);
+
+	return (
+		<div className={descriptionClasses} style={style}>
+			<TooltipHost {...tooltipProps}>
+				<button
+					type="button"
+					className={styles.descriptionIcon}
+					aria-label="Additional information"
+					tabIndex={0}
+				>
+					<Icon iconName={icon} />
+				</button>
+			</TooltipHost>
+		</div>
+	);
+});
+
+Description.displayName = 'Description';
