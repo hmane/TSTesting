d1d4c182b3f5be3c9eceddefd3092d429c237169
Hemant Mane <hemant.u.mane@gmail.com>
2025-10-01 15:20:15 -0700

    Add index.ts to export various schemas

---
+++ form/resolvers/index.ts
@@ -0,0 +1,36 @@
+// Request Info Schemas
+export {
+  requestInfoSchema,
+  saveAsDraftRequestInfoSchema,
+  submitRequestInfoSchema,
+} from './requestInfoSchema';
+
+// Approvals Schemas
+export {
+  approvalsSchema,
+  saveAsDraftApprovalsSchema,
+  submitApprovalsSchema,
+} from './approvalsSchema';
+
+// Legal Intake Schemas
+export {
+  assignAttorneySchema,
+  submitToCommitteeSchema,
+} from './legalIntakeSchema';
+
+// Compliance Review Schemas
+export {
+  submitComplianceReviewSchema,
+  saveComplianceProgressSchema,
+} from './complianceReviewSchema';
+
+// Closeout Schema
+export {
+  closeoutSchema,
+} from './closeoutSchema';
+
+// Hold and Cancel Schemas
+export {
+  holdRequestSchema,
+  cancelRequestSchema,
+} from './holdAndCancelSchemas';
